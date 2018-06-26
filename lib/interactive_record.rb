e287fef074fd67ba9c2a2fcde861a1d0cc897092
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-26 23:48:59 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,6 +51,7 @@ class InteractiveRecord
   end
   
   def self.find_by(x)
+    binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE #{x.to_a[0]} = '#{x.to_a[1]}'"
     DB[:conn].execute(sql)
   end
